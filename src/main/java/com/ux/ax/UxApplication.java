efe1839bd9608ffecc76e869a7a682da67ce0d05
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2020-04-28 11:49:39 +0100

    Initial commit from Atomist

---
+++ src/main/java/com/ux/ax/UxApplication.java
@@ -0,0 +1,12 @@
+package com.ux.ax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UxApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ux/ax/UxController.java
@@ -0,0 +1,15 @@
+package com.ux.ax;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class UxController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
